72edcde0eea1f09f420b74c637880f279d3a80a3
ExceptionEvader <evnx32@gmail.com>
2023-08-09 21:18:31 +0700

    Update AppConstant.kt

--- app/src/main/java/id/blossom/utils/AppConstant.kt
+++ app/src/main/java/id/blossom/utils/AppConstant.kt
@@ -4,7 +4,5 @@ object AppConstant {
     const val VIEW_TYPE_ITEM = 0
     const val VIEW_TYPE_LOADING = 1
     const val ALL = "all"
-    const val API_KEY = "9f6482a584804376874b848980b7a044"
-    const val COUNTRY = "us"
     const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
 }
